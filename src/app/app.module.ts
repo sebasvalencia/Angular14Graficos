7c71680a4dc897f2d55a0dfeb02337b7f70a4951
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-02-28 18:57:57 -0500

    Catorceava app en Angular - Graficas dinamicas - Instalaciones necesarias ng2-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
 
+// Graficos
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
