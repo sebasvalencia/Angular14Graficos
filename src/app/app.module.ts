33d563367d4c395995da3f476b8b6d20ece492d4
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-02-28 19:53:51 -0500

    Catorceava app en Angular - Graficas dinamicas - Grafica de radar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { LineaComponent } from './components/linea/linea.component';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { BarraComponent } from './components/barra/barra.component';
 import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LineaComponent,
     BarraComponent,
-    DonaComponent
+    DonaComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule,
